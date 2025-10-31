105c43cd78c3bba5f1407c5adea36857594485a6
blackdandan <784818984@qq.com>
2025-11-01 04:14:27 +0800

    记录知识库，并把知识库放在第四位，增加Swagger文档

--- projects-server/common-lib/build.gradle.kts
+++ projects-server/common-lib/build.gradle.kts
@@ -37,6 +37,9 @@ dependencies {
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
     
+    // SpringDoc OpenAPI
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
+    
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
